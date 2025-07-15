02d9b845b11b36d27f821e260734696d8cbf5632
Utkarsh Agrawal <149224355+utkarsh0830@users.noreply.github.com>
2025-07-15 22:42:39 +0530

    Function Based Views all RestApi Completed

--- drf_tutorial/api/urls.py
+++ drf_tutorial/api/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('items/',views.ItemView)
-    
+    path('items/',views.ItemView),
+    path('items/<int:pk>',views.ItemDetailView)
 ]
--- drf_tutorial/api/views.py
+++ drf_tutorial/api/views.py
@@ -8,7 +8,7 @@ from rest_framework.decorators import api_view
 
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def ItemView(request):
     if request.method == 'GET':
         items = Item.objects.all()
@@ -17,11 +17,35 @@ def ItemView(request):
     elif request.method == 'POST':
         date = request.data
         serializer = ItemSerializer(data=date)
-        if(serializer.is_valid):
+        if(serializer.is_valid()):
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['GET','PUT','DELETE'])
+def ItemDetailView(request,pk):
+    try:
+        item = Item.objects.get(pk=pk)
+    except Item.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = ItemSerializer(item)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = ItemSerializer(item,data=request.data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+
         
 
     
