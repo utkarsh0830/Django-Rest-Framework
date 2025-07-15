295488a140cda8042975a05bf9f81f0e280e4cd3
Utkarsh Agrawal <149224355+utkarsh0830@users.noreply.github.com>
2025-07-15 23:03:20 +0530

    Commented

--- drf_tutorial/api/views.py
+++ drf_tutorial/api/views.py
@@ -6,45 +6,48 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 
-
-# Create your views here.
-# @api_view(['GET','POST'])
-# def ItemView(request):
-#     if request.method == 'GET':
-#         items = Item.objects.all()
-#         serializer = ItemSerializer(items,many=True)
-#         return Response(serializer.data,status=status.HTTP_200_OK)
-#     elif request.method == 'POST':
-#         date = request.data
-#         serializer = ItemSerializer(data=date)
-#         if(serializer.is_valid()):
-#             serializer.save()
-#             return Response(serializer.data,status=status.HTTP_201_CREATED)
+# Function Based API Views
+"""
+@api_view(['GET','POST'])
+def ItemView(request):
+    if request.method == 'GET':
+        items = Item.objects.all()
+        serializer = ItemSerializer(items,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        date = request.data
+        serializer = ItemSerializer(data=date)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
         
-#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
 
-# @api_view(['GET','PUT','DELETE'])
-# def ItemDetailView(request,pk):
-#     try:
-#         item = Item.objects.get(pk=pk)
-#     except Item.DoesNotExist:
-#         return Response(status = status.HTTP_404_NOT_FOUND)
+@api_view(['GET','PUT','DELETE'])
+def ItemDetailView(request,pk):
+    try:
+        item = Item.objects.get(pk=pk)
+    except Item.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
     
-#     if request.method == 'GET':
-#         serializer = ItemSerializer(item)
-#         return Response(serializer.data,status=status.HTTP_200_OK)
-#     elif request.method == 'PUT':
-#         serializer = ItemSerializer(item,data=request.data)
-#         if(serializer.is_valid()):
-#             serializer.save()
-#             return Response(serializer.data,status=status.HTTP_200_OK)
-#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-#     elif request.method == 'DELETE':
-#         item.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
+    if request.method == 'GET':
+        serializer = ItemSerializer(item)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = ItemSerializer(item,data=request.data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
+"""
 
+#Class Based API View
+"""
 class ItemView(APIView):
     def get(self,request):
         items = Item.objects.all()
@@ -88,7 +91,7 @@ class ItemDetailView(APIView):
         item.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    
+ """
 
         
 
